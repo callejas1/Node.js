c15d7186e17268b217bb65b4ec1b804741e43a3d
Yoselyn Callejas <yoselyncallejas19@gmail.com>
2020-12-05 00:59:51 +0100

    Completed 4 node.js exercises

--- week3/homework/exercises/4-handlebars/script.js
+++ week3/homework/exercises/4-handlebars/script.js
@@ -1,28 +1,14 @@
-
 /**
  * 4. Fun with Handlebars
- * 
+ *
  * Write a javascript function that simulates playing the game cards against humanity.
  * The code should choose a subject and a punchline at random,
  * then replace them in a sentece using handlebars.
- * 
+ *
  * Hints:
  * - Check the handlebars npm page for examples and documentation
  */
-
-
-function drawCard() {
-  // YOUR CODE GOES IN HERE
-}
-
-drawCard();
-
-/**
- * Given an array, return an element from it chosen at random
- */
-function getRandomElement(array) {
-  // YOUR CODE GOES IN HERE
-}
+'use strict';
 
 const subjects = [
   'shark',
@@ -43,3 +29,27 @@ const punchlines = [
   'achieve world piece',
   'help people learn programing',
 ];
+
+const Handlebars = require('handlebars');
+
+function drawCard() {
+  // YOUR CODE GOES IN HERE
+  const cardData = {
+    subject: getRandomElement(subjects),
+    punchline: getRandomElement(punchlines),
+  };
+  const card = `${cardData.subject} is great to ${cardData.punchline}`;
+  const template = Handlebars.compile(card);
+  const result = template(cardData);
+  console.log(result);
+}
+
+drawCard();
+
+/**
+ * Given an array, return an element from it chosen at random
+ */
+function getRandomElement(array) {
+  // YOUR CODE GOES IN HERE
+  return array[Math.floor(Math.random() * array.length)];
+}
